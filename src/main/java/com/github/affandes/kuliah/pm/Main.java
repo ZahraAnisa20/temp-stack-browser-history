221afda43ce5b5be25d98c52c11df55fd56852da
ZahraAnisa20 <12350121727@students.uin-suska.ac.id>
2024-10-30 04:45:24 +0700

    Update Main.java

--- src/main/java/com/github/affandes/kuliah/pm/Main.java
+++ src/main/java/com/github/affandes/kuliah/pm/Main.java
@@ -1,8 +1,54 @@
 package com.github.affandes.kuliah.pm;
+class Song {
+    String title;
+    Song next;
+public class Main1 {
+    public Song(String title) {
+        this.title = title;
+        this.next = null;
+    }
+}
 
-public class Main {
+class Playlist {
+    Song head;
+public class Main1 {
     public static void main(String[] args) {
-        // Tulis kode disini
+    public Playlist() {
+        this.head = null;
+    }
 
+    public void addSong(String title) {
+        Song newSong = new Song(title);
+        if (head == null) {
+            head = newSong;
+        } else {
+            Song current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newSong;
+        }
+    }
+
+    public void displayPlaylist() {
+        if (head == null) {
+            System.out.println("Playlist is empty.");
+        } else {
+            Song current = head;
+            while (current != null) {
+                System.out.println(current.title);
+                current = current.next;
+            }
+        }
+    }
+}
+
+public class Main1 {
+    public static void main(String[] args) {
+        Playlist playlist = new Playlist();
+        playlist.addSong("Song 1");
+        playlist.addSong("Song 2");
+        playlist.addSong("Song 3");
+        playlist.displayPlaylist();
     }
 }

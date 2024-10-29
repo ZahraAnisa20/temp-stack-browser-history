292424f51c57ed97dede12d88c7164e67162fe79
ZahraAnisa20 <12350121727@students.uin-suska.ac.id>
2024-10-29 22:40:54 +0700

    Zahra.java

--- src/main/java/com/github/affandes/kuliah/pm/Main.java
+++ src/main/java/com/github/affandes/kuliah/pm/Main.java
@@ -2,7 +2,7 @@ package com.github.affandes.kuliah.pm;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-public class Main {
+public class Main { 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         List<String> history = new ArrayList<>();
